30ef844513f9669021d6adb3c74b14a3eb3b2560
guncow0603 <guncow0603@gmail.com>
2023-10-13 20:27:14 +0900

    비교연산자

---
+++ src/secondJava/JavaStudy03.java
@@ -0,0 +1,17 @@
+package secondJava;
+
+public class JavaStudy03 {
+    public static void main(String[] args) {
+        //비교 연산자 예제
+
+        System.out.println(10 > 9); // 10이 9보다 크니? :응!(true)
+        System.out.println(10 >= 9); // 10이 9보다 크거나 같니? :응!(true)
+        System.out.println(10 < 9); // 10이 9보다 작니? :응!(false)
+        System.out.println(10 <= 9); // 10이 9보다 작거나 같니? :응!(false)
+        System.out.println(10 == 10); // 10이 10이랑 같니? :응!(true)
+        System.out.println(10 == 9); // 10이 9랑 같니? :응!(false)
+        System.out.println(10 != 10); // 10은 10이 아니니? :응!(false)
+        System.out.println(10 != 9); // 10은 9가 아니니? :응!(true)
+
+    }
+}
