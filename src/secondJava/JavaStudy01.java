17ab9c115e682de3f8c32426d21f24f8f752371f
guncow0603 <guncow0603@gmail.com>
2023-10-13 20:21:35 +0900

    연산자

---
+++ src/secondJava/JavaStudy01.java
@@ -0,0 +1,20 @@
+package secondJava;
+
+public class JavaStudy01 {
+    public static void main(String[] args) {
+        // X + Y = Z
+        int x = 5;
+        int y = 10;
+
+        int z = x + y; // 5(피연산자) +(연산자) 10(피연산자)
+
+        System.out.println(z);
+
+        //산술연산자 : +, -, *, /, %(나머지), <<, >>
+        //비교연산자 : >, <, >==, <=, ==, !=
+        //논리연산자 : &&, ||, !
+        //대입연산자 : =, ++, --
+        //기타연산자 : (type), ? :, instance of
+
+    }
+}
