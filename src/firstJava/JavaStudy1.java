495a7904e81dd2cecbfa8b7925f40eeb0569e68f
guncow0603 <guncow0603@gmail.com>
2023-10-13 20:04:40 +0900

    숫자 -> 문자

---
+++ src/firstJava/JavaStudy1.java
@@ -0,0 +1,17 @@
+package firstJava;// 숫자 -> 문자
+//import : im(in) + port(항구)
+import java.util.Scanner;
+
+public class JavaStudy1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int asciiNumber = sc.nextInt();
+        char ch = (char)asciiNumber; // 문자로 " 형변환" 을 해주면 숫자에 맞는 문자로 표현됩니다.
+
+        System.out.println(ch);
+
+    }
+
+}
