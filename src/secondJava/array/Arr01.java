b2fb0fea2a21e097b091e6da06330f69fe9b5326
guncow0603 <guncow0603@gmail.com>
2023-10-13 20:52:07 +0900

    배열 선언

---
+++ src/secondJava/array/Arr01.java
@@ -0,0 +1,23 @@
+package secondJava.array;
+
+public class Arr01 {
+    public static void main(String[] args) {
+        // 배열 선언
+        // 일반적인 선언방식
+        int[] intArray;    // 정수 배열
+        long[] longArray;
+        double[] doubleArray;  // 실수 배열
+        char[] charArray;  // 문자 배열
+        String[] stringArray; // 문자열 배열
+
+
+
+        // 배열 선언
+        // 2번째 선언방식
+        int intArray1[];    // 정수 배열
+        long longArray1[];
+        double doubleArray1[];  // 실수 배열
+        char charArray1[];  // 문자 배열
+        String stringArray1[]; // 문자열 배열
+    }
+}
