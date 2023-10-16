935c6b4a5594bc90fb9862b6e78d96535b30ee28
guncow0603 <guncow0603@gmail.com>
2023-10-16 18:29:25 +0900

    package

--- src/thirdJava/packageExample/main/main.java
+++ src/thirdJava/packageExample/main/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
         // 클래스의 일부분이며, 하위 패키지를 도트(.)로 구분한다.
         thirdJava.packageExample.pk01.Car car1 = new thirdJava.packageExample.pk01.Car();
-        thirdJava.packageExample.pk01.Car car2 = new thirdJava.packageExample.pk01.Car();
+        thirdJava.packageExample.pk02.Car car2 = new thirdJava.packageExample.pk02.Car();
 
         car1.horn();
         car2.horn();
