f095a854a8fa1e871eba0b6e938b710c518b98e7
guncow0603 <guncow0603@gmail.com>
2023-10-16 20:26:22 +0900

    추상 클래스

---
+++ src/thirdJava/abs/AudiCar.java
@@ -0,0 +1,9 @@
+package thirdJava.abs;
+
+public class AudiCar extends Car {
+
+    @Override
+    public void horn() {
+        System.out.println("Audi 빵빵");
+    }
+}
---
+++ src/thirdJava/abs/GenesisCar.java
@@ -0,0 +1,9 @@
+package thirdJava.abs;
+
+public class GenesisCar extends Car {
+
+    @Override
+    public void horn() {
+        System.out.println("genesis 빵빵");
+    }
+}
