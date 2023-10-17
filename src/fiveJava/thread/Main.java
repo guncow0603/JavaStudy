65a76b09c6f71e4d106700fcb429f819eeacfbe2
guncow0603 <guncow0603@gmail.com>
2023-10-17 16:05:58 +0900

    람다식

--- src/fiveJava/thread/Main.java
+++ src/fiveJava/thread/Main.java
@@ -2,12 +2,22 @@ package fiveJava.thread;
 
 public class Main {
     public static void main(String[] args) {
-//        TestThread thread = new TestThread();
-//        thread.start();
-        Runnable run = new TestRunnable();
-        Thread thread = new Thread(run); // 쓰레드 생성
+        Runnable task = () -> {
+            int sum = 0;
+            for (int i = 0; i < 50; i++) {
+                sum += i;
+                System.out.println(sum);
+            }
+            System.out.println(Thread.currentThread().getName() + " 최종 합 : " + sum);
+        };
 
-        thread.start();
+        Thread thread1 = new Thread(task);
+        thread1.setName("thread1");
+        Thread thread2 = new Thread(task);
+        thread2.setName("thread2");
+
+        thread1.start();
+        thread2.start();
     }
 }
 
