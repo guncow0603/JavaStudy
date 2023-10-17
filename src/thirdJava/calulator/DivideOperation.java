df2cdf13afccda8a5452e7f8f0f22bc400747ee5
guncow0603 <guncow0603@gmail.com>
2023-10-17 09:57:45 +0900

    계산기 만들기

---
+++ src/thirdJava/calulator/DivideOperation.java
@@ -0,0 +1,8 @@
+package thirdJava.calulator;
+
+public class DivideOperation extends AbstractOperation{
+    @Override
+    public double operate(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
---
+++ src/thirdJava/calulator/MultiplyOperation.java
@@ -0,0 +1,8 @@
+package thirdJava.calulator;
+
+public class MultiplyOperation extends AbstractOperation{
+    @Override
+    public double operate(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+}
