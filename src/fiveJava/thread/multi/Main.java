254bccedc1ae35dd3e3b949452cc827df780b1fb
guncow0603 <guncow0603@gmail.com>
2023-10-17 16:12:27 +0900

    단일쓰레드와 멀티쓰레드

---
+++ src/fiveJava/thread/multi/Main.java
@@ -0,0 +1,29 @@
+package fiveJava.thread.multi;
+
+public class Main {
+    //예측 할 수 없다.
+    public static void main(String[] args) {
+
+       //1st
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.print("$");
+            }
+        };
+        //2nd
+        Runnable task2 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.print("*");
+            }
+        };
+
+
+        Thread thread1 = new Thread(task);
+        thread1.setName("thread1");
+        Thread thread2 = new Thread(task2);
+        thread2.setName("thread2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
